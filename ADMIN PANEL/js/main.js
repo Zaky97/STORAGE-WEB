97ada66f9904eb0d6900e265e339884bba97c242
Zaky97 <zaky7645@gmail.com>
2023-06-06 22:45:33 +0700

    modified:   ADMIN PANEL/js/main.js 	modified:   js/main.js

--- ADMIN PANEL/js/main.js
+++ ADMIN PANEL/js/main.js
@@ -1,144 +1,234 @@
-// Initialize Firebase
+const _0x36d639 = _0x53af;
+(function (_0x446315, _0x53dba1) {
+  const _0x2a3adb = _0x53af,
+    _0x284cef = _0x446315();
+  while (!![]) {
+    try {
+      const _0x404710 =
+        (-parseInt(_0x2a3adb(0x107)) / 0x1) *
+          (-parseInt(_0x2a3adb(0x104)) / 0x2) +
+        (-parseInt(_0x2a3adb(0xf0)) / 0x3) * (parseInt(_0x2a3adb(0xd3)) / 0x4) +
+        parseInt(_0x2a3adb(0xcd)) / 0x5 +
+        -parseInt(_0x2a3adb(0xd8)) / 0x6 +
+        (parseInt(_0x2a3adb(0xde)) / 0x7) * (-parseInt(_0x2a3adb(0xcc)) / 0x8) +
+        -parseInt(_0x2a3adb(0x100)) / 0x9 +
+        (parseInt(_0x2a3adb(0xd7)) / 0xa) * (parseInt(_0x2a3adb(0xca)) / 0xb);
+      if (_0x404710 === _0x53dba1) break;
+      else _0x284cef["push"](_0x284cef["shift"]());
+    } catch (_0x1b9dbb) {
+      _0x284cef["push"](_0x284cef["shift"]());
+    }
+  }
+})(_0xe504, 0x25bc1);
 const firebaseConfig = {
-  // Your Firebase configuration
-  apiKey: "AIzaSyBaEE0Gx3iMoLWDAUTSLCoLLWiIJUljWvk",
-  authDomain: "zaky-50e47.firebaseapp.com",
-  databaseURL: "https://zaky-50e47-default-rtdb.firebaseio.com",
-  projectId: "zaky-50e47",
+  apiKey: _0x36d639(0xe5),
+  authDomain: _0x36d639(0xf3),
+  databaseURL: _0x36d639(0x106),
+  projectId: _0x36d639(0x102),
   storageBucket: "zaky-50e47.appspot.com",
   messagingSenderId: "1038515986869",
-  appId: "1:1038515986869:web:7fa6249a5ff22acb96b9fc",
+  appId: _0x36d639(0xd4),
   measurementId: "G-JD159TTZNG",
 };
-
-firebase.initializeApp(firebaseConfig);
-
-// Create a storage reference
-const storageRef = firebase.storage().ref();
-
-// Get file list from Firebase Firestore
-function getFileList() {
-  const fileListElement = document.getElementById("file-list");
-  fileListElement.innerHTML = "";
-
-  firebase
-    .firestore()
-    .collection("files")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const fileData = doc.data();
-        const fileId = doc.id;
-        const fileName = fileData.fileName;
-        const category = fileData.category;
-
-        // Create download button
-        const downloadButton = document.createElement("button");
-        downloadButton.innerText = "Download";
-        downloadButton.className = "btn btn-success";
-        downloadButton.addEventListener("click", () => {
-          storageRef
-            .child(fileName)
-            .getDownloadURL()
-            .then((url) => {
-              window.open(url, "_blank");
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        });
-
-        // Create delete button
-        const deleteButton = document.createElement("button");
-        deleteButton.innerText = "Delete";
-        deleteButton.className = "btn btn-danger";
-        deleteButton.addEventListener("click", () => {
-          // Delete file from storage
-          storageRef
-            .child(fileName)
-            .delete()
-            .then(() => {
-              // Delete file data from Firestore
-              firebase.firestore().collection("files").doc(fileId).delete();
-              getFileList();
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        });
-
-        // Create table row
-        const row = fileListElement.insertRow();
-        const cell1 = row.insertCell();
-        const cell2 = row.insertCell();
-        const cell3 = row.insertCell();
-        const cell4 = row.insertCell();
-
-        cell1.textContent = fileName;
-        cell2.textContent = category;
-        cell3.appendChild(downloadButton);
-        cell4.appendChild(deleteButton);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+firebase["initializeApp"](firebaseConfig);
+function _0x53af(_0x46eee7, _0x17c0fe) {
+  const _0xe504fd = _0xe504();
+  return (
+    (_0x53af = function (_0x53af4e, _0x1111bf) {
+      _0x53af4e = _0x53af4e - 0xc8;
+      let _0xefa0f6 = _0xe504fd[_0x53af4e];
+      return _0xefa0f6;
+    }),
+    _0x53af(_0x46eee7, _0x17c0fe)
+  );
 }
-
-// Search file by name and category
-const searchInput = document.getElementById("search-input");
-searchInput.addEventListener("input", () => {
-  const searchValue = searchInput.value.toLowerCase();
-  const fileList = document.querySelectorAll("#file-list tr");
-
-  fileList.forEach((row) => {
-    const fileName = row.cells[0].textContent.toLowerCase();
-    const category = row.cells[1].textContent.toLowerCase();
-
-    if (fileName.includes(searchValue) || category.includes(searchValue)) {
-      row.style.display = "table-row";
-    } else {
-      row.style.display = "none";
-    }
+const storageRef = firebase[_0x36d639(0xe8)]()[_0x36d639(0xf6)]();
+function getFileList() {
+  const _0x419fa6 = _0x36d639,
+    _0x17538f = document["getElementById"]("file-list");
+  (_0x17538f[_0x419fa6(0xe2)] = ""),
+    firebase[_0x419fa6(0xfb)]()
+      [_0x419fa6(0xf2)](_0x419fa6(0xfe))
+      ["get"]()
+      [_0x419fa6(0xea)]((_0x402ffc) => {
+        _0x402ffc["forEach"]((_0x305c55) => {
+          const _0x479549 = _0x53af,
+            _0x5bcb5d = _0x305c55[_0x479549(0xe3)](),
+            _0x29c0b3 = _0x305c55["id"],
+            _0x27c82d = _0x5bcb5d["fileName"],
+            _0x5c5dd1 = _0x5bcb5d[_0x479549(0xef)],
+            _0x10357d = document[_0x479549(0xed)](_0x479549(0x108));
+          (_0x10357d["innerText"] = _0x479549(0xfc)),
+            (_0x10357d[_0x479549(0xe1)] = _0x479549(0xe4)),
+            _0x10357d[_0x479549(0x103)](_0x479549(0xe7), () => {
+              const _0x1cdcd2 = _0x479549;
+              storageRef["child"](_0x27c82d)
+                [_0x1cdcd2(0xeb)]()
+                [_0x1cdcd2(0xea)]((_0x1bede7) => {
+                  const _0xce9ad4 = _0x1cdcd2;
+                  window[_0xce9ad4(0xf5)](_0x1bede7, _0xce9ad4(0xf7));
+                })
+                [_0x1cdcd2(0xc9)]((_0xc915ab) => {
+                  const _0x45c5a1 = _0x1cdcd2;
+                  console[_0x45c5a1(0xdc)](_0xc915ab);
+                });
+            });
+          const _0x45ccb0 = document["createElement"](_0x479549(0x108));
+          (_0x45ccb0[_0x479549(0x101)] = "Delete"),
+            (_0x45ccb0[_0x479549(0xe1)] = _0x479549(0xd9)),
+            _0x45ccb0["addEventListener"](_0x479549(0xe7), () => {
+              const _0x51bb7f = _0x479549;
+              storageRef[_0x51bb7f(0xc8)](_0x27c82d)
+                ["delete"]()
+                ["then"](() => {
+                  const _0x4c6864 = _0x51bb7f;
+                  firebase[_0x4c6864(0xfb)]()
+                    [_0x4c6864(0xf2)](_0x4c6864(0xfe))
+                    [_0x4c6864(0xec)](_0x29c0b3)
+                    [_0x4c6864(0xfa)](),
+                    getFileList();
+                })
+                [_0x51bb7f(0xc9)]((_0x238a5b) => {
+                  console["log"](_0x238a5b);
+                });
+            });
+          const _0x3d0e83 = _0x17538f[_0x479549(0xcf)](),
+            _0x433762 = _0x3d0e83[_0x479549(0xfd)](),
+            _0x5a3120 = _0x3d0e83[_0x479549(0xfd)](),
+            _0x4c03e3 = _0x3d0e83[_0x479549(0xfd)](),
+            _0x4ab682 = _0x3d0e83[_0x479549(0xfd)]();
+          (_0x433762[_0x479549(0xcb)] = _0x27c82d),
+            (_0x5a3120[_0x479549(0xcb)] = _0x5c5dd1),
+            _0x4c03e3["appendChild"](_0x10357d),
+            _0x4ab682[_0x479549(0xff)](_0x45ccb0);
+        });
+      })
+      [_0x419fa6(0xc9)]((_0xf96e84) => {
+        const _0x278546 = _0x419fa6;
+        console[_0x278546(0xdc)](_0xf96e84);
+      });
+}
+const searchInput = document[_0x36d639(0xee)](_0x36d639(0xd1));
+function _0xe504() {
+  const _0x1af3cb = [
+    "then",
+    "getDownloadURL",
+    "doc",
+    "createElement",
+    "getElementById",
+    "category",
+    "611337ODTPly",
+    "value",
+    "collection",
+    "zaky-50e47.firebaseapp.com",
+    "preventDefault",
+    "open",
+    "ref",
+    "_blank",
+    "file-input",
+    "table-row",
+    "delete",
+    "firestore",
+    "Download",
+    "insertCell",
+    "files",
+    "appendChild",
+    "2098701vNpXCn",
+    "innerText",
+    "zaky-50e47",
+    "addEventListener",
+    "32CyVQzG",
+    "change",
+    "https://zaky-50e47-default-rtdb.firebaseio.com",
+    "2503xnndTG",
+    "button",
+    "child",
+    "catch",
+    "11PznDPR",
+    "textContent",
+    "17128tEQdqD",
+    "1401250zvqwCV",
+    "Choose\x20file",
+    "insertRow",
+    "toLowerCase",
+    "search-input",
+    "#file-list\x20tr",
+    "4xWRshP",
+    "1:1038515986869:web:7fa6249a5ff22acb96b9fc",
+    "style",
+    "add",
+    "5449300dzHMeR",
+    "1603656jwSGEf",
+    "btn\x20btn-danger",
+    "display",
+    "forEach",
+    "log",
+    "cells",
+    "21LMnrnf",
+    "none",
+    "put",
+    "className",
+    "innerHTML",
+    "data",
+    "btn\x20btn-success",
+    "AIzaSyBaEE0Gx3iMoLWDAUTSLCoLLWiIJUljWvk",
+    "name",
+    "click",
+    "storage",
+    "includes",
+  ];
+  _0xe504 = function () {
+    return _0x1af3cb;
+  };
+  return _0xe504();
+}
+searchInput[_0x36d639(0x103)]("input", () => {
+  const _0x189e44 = _0x36d639,
+    _0x1c1bc8 = searchInput[_0x189e44(0xf1)][_0x189e44(0xd0)](),
+    _0x3db6f9 = document["querySelectorAll"](_0x189e44(0xd2));
+  _0x3db6f9[_0x189e44(0xdb)]((_0x52e172) => {
+    const _0x33002a = _0x189e44,
+      _0x4a1c10 =
+        _0x52e172[_0x33002a(0xdd)][0x0]["textContent"]["toLowerCase"](),
+      _0x14697d = _0x52e172["cells"][0x1][_0x33002a(0xcb)]["toLowerCase"]();
+    _0x4a1c10[_0x33002a(0xe9)](_0x1c1bc8) ||
+    _0x14697d[_0x33002a(0xe9)](_0x1c1bc8)
+      ? (_0x52e172[_0x33002a(0xd5)]["display"] = _0x33002a(0xf9))
+      : (_0x52e172[_0x33002a(0xd5)][_0x33002a(0xda)] = _0x33002a(0xdf));
   });
 });
-
-// Handle file upload
-const uploadForm = document.getElementById("upload-form");
-const fileInput = document.getElementById("file-input");
-const categoryInput = document.getElementById("category-input");
-const fileInputLabel = document.querySelector(".custom-file-label");
-
-fileInput.addEventListener("change", () => {
-  fileInputLabel.textContent = fileInput.files[0].name;
-});
-
-uploadForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const file = fileInput.files[0];
-  const fileName = file.name;
-  const category = categoryInput.value;
-  const fileRef = storageRef.child(fileName);
-
-  fileRef
-    .put(file)
-    .then(() => {
-      // Save the file metadata including the category to a database
-      return firebase.firestore().collection("files").add({
-        fileName,
-        category,
+const uploadForm = document[_0x36d639(0xee)]("upload-form"),
+  fileInput = document[_0x36d639(0xee)](_0x36d639(0xf8)),
+  categoryInput = document[_0x36d639(0xee)]("category-input"),
+  fileInputLabel = document["querySelector"](".custom-file-label");
+fileInput["addEventListener"](_0x36d639(0x105), () => {
+  const _0x363f3a = _0x36d639;
+  fileInputLabel["textContent"] =
+    fileInput[_0x363f3a(0xfe)][0x0][_0x363f3a(0xe6)];
+}),
+  uploadForm[_0x36d639(0x103)]("submit", (_0xc93319) => {
+    const _0x4708fe = _0x36d639;
+    _0xc93319[_0x4708fe(0xf4)]();
+    const _0x3ca751 = fileInput["files"][0x0],
+      _0x35fb5e = _0x3ca751["name"],
+      _0x265a7e = categoryInput["value"],
+      _0xeb17c8 = storageRef[_0x4708fe(0xc8)](_0x35fb5e);
+    _0xeb17c8[_0x4708fe(0xe0)](_0x3ca751)
+      [_0x4708fe(0xea)](() => {
+        const _0x1b67c9 = _0x4708fe;
+        return firebase["firestore"]()
+          [_0x1b67c9(0xf2)](_0x1b67c9(0xfe))
+          [_0x1b67c9(0xd6)]({ fileName: _0x35fb5e, category: _0x265a7e });
+      })
+      [_0x4708fe(0xea)](() => {
+        const _0x2bca4e = _0x4708fe;
+        getFileList(),
+          (fileInput["value"] = ""),
+          (fileInputLabel[_0x2bca4e(0xcb)] = _0x2bca4e(0xce)),
+          (categoryInput["value"] = "");
+      })
+      [_0x4708fe(0xc9)]((_0x570fa2) => {
+        console["log"](_0x570fa2);
       });
-    })
-    .then(() => {
-      getFileList();
-      fileInput.value = "";
-      fileInputLabel.textContent = "Choose file";
-      categoryInput.value = "";
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-// Call getFileList() on page load
-getFileList();
+  }),
+  getFileList();
--- js/main.js
+++ js/main.js
@@ -1,85 +1,168 @@
-// Initialize Firebase
-const firebaseConfig = {
-  // Your Firebase configuration
-  apiKey: "AIzaSyBaEE0Gx3iMoLWDAUTSLCoLLWiIJUljWvk",
-  authDomain: "zaky-50e47.firebaseapp.com",
-  databaseURL: "https://zaky-50e47-default-rtdb.firebaseio.com",
-  projectId: "zaky-50e47",
-  storageBucket: "zaky-50e47.appspot.com",
-  messagingSenderId: "1038515986869",
-  appId: "1:1038515986869:web:7fa6249a5ff22acb96b9fc",
-  measurementId: "G-JD159TTZNG",
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Create a storage reference
-const storageRef = firebase.storage().ref();
-
-// Get file list from Firebase Firestore
-function getFileList() {
-  const fileListElement = document.getElementById("file-list");
-  fileListElement.innerHTML = "";
-
-  firebase
-    .firestore()
-    .collection("files")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const fileData = doc.data();
-        const fileName = fileData.fileName;
-        const category = fileData.category;
-
-        // Create download button
-        const downloadButton = document.createElement("button");
-        downloadButton.innerText = "Download";
-        downloadButton.className = "btn btn-success";
-        downloadButton.addEventListener("click", () => {
-          storageRef
-            .child(fileName)
-            .getDownloadURL()
-            .then((url) => {
-              window.open(url, "_blank");
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        });
-
-        // Create table row
-        const row = fileListElement.insertRow();
-        const cell1 = row.insertCell();
-        const cell2 = row.insertCell();
-        const cell3 = row.insertCell();
-
-        cell1.textContent = fileName;
-        cell2.textContent = category;
-        cell3.appendChild(downloadButton);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-// Search file by name
-const searchInput = document.getElementById("search-input");
-searchInput.addEventListener("input", () => {
-  const searchValue = searchInput.value.toLowerCase();
-  const fileList = document.querySelectorAll("#file-list tr");
-
-  fileList.forEach((row) => {
-    const fileName = row.cells[0].textContent.toLowerCase();
-    const category = row.cells[1].textContent.toLowerCase();
-
-    if (fileName.includes(searchValue) || category.includes(searchValue)) {
-      row.style.display = "table-row";
-    } else {
-      row.style.display = "none";
+const _0x19d78b = _0x5799;
+(function (_0x3af6f8, _0x2ad2e1) {
+  const _0x2b3c05 = _0x5799,
+    _0x3c6cec = _0x3af6f8();
+  while (!![]) {
+    try {
+      const _0x14935b =
+        parseInt(_0x2b3c05(0x21e)) / 0x1 +
+        (-parseInt(_0x2b3c05(0x1fa)) / 0x2) *
+          (-parseInt(_0x2b3c05(0x20f)) / 0x3) +
+        (-parseInt(_0x2b3c05(0x20a)) / 0x4) *
+          (parseInt(_0x2b3c05(0x221)) / 0x5) +
+        parseInt(_0x2b3c05(0x214)) / 0x6 +
+        (parseInt(_0x2b3c05(0x1ed)) / 0x7) *
+          (parseInt(_0x2b3c05(0x211)) / 0x8) +
+        (-parseInt(_0x2b3c05(0x20c)) / 0x9) *
+          (-parseInt(_0x2b3c05(0x216)) / 0xa) +
+        -parseInt(_0x2b3c05(0x217)) / 0xb;
+      if (_0x14935b === _0x2ad2e1) break;
+      else _0x3c6cec["push"](_0x3c6cec["shift"]());
+    } catch (_0x215783) {
+      _0x3c6cec["push"](_0x3c6cec["shift"]());
     }
+  }
+})(_0x481e, 0x87271);
+const firebaseConfig = {
+  apiKey: _0x19d78b(0x218),
+  authDomain: "zaky-50e47.firebaseapp.com",
+  databaseURL: _0x19d78b(0x204),
+  projectId: _0x19d78b(0x20b),
+  storageBucket: _0x19d78b(0x1fc),
+  messagingSenderId: _0x19d78b(0x1fb),
+  appId: _0x19d78b(0x213),
+  measurementId: _0x19d78b(0x1f3),
+};
+firebase[_0x19d78b(0x1ee)](firebaseConfig);
+function _0x5799(_0x27e8f8, _0x396fb6) {
+  const _0x481e16 = _0x481e();
+  return (
+    (_0x5799 = function (_0x579962, _0x433aac) {
+      _0x579962 = _0x579962 - 0x1ed;
+      let _0x21bc91 = _0x481e16[_0x579962];
+      return _0x21bc91;
+    }),
+    _0x5799(_0x27e8f8, _0x396fb6)
+  );
+}
+function _0x481e() {
+  const _0x705be9 = [
+    "forEach",
+    "category",
+    "appendChild",
+    "cells",
+    "fileName",
+    "2DxNFxY",
+    "1038515986869",
+    "zaky-50e47.appspot.com",
+    "collection",
+    "get",
+    "insertCell",
+    "className",
+    "style",
+    "button",
+    "_blank",
+    "https://zaky-50e47-default-rtdb.firebaseio.com",
+    "getElementById",
+    "toLowerCase",
+    "catch",
+    "log",
+    "innerText",
+    "18296UUbFFN",
+    "zaky-50e47",
+    "27jAeNPX",
+    "then",
+    "querySelectorAll",
+    "1373259qNmycq",
+    "table-row",
+    "544hwWRvb",
+    "value",
+    "1:1038515986869:web:7fa6249a5ff22acb96b9fc",
+    "1362594VmQxmZ",
+    "child",
+    "1678780xWGkiD",
+    "20858915ABlgTo",
+    "AIzaSyBaEE0Gx3iMoLWDAUTSLCoLLWiIJUljWvk",
+    "data",
+    "ref",
+    "Download",
+    "innerHTML",
+    "open",
+    "839912dyKueC",
+    "display",
+    "includes",
+    "570cOrfNv",
+    "97062pMpRba",
+    "initializeApp",
+    "addEventListener",
+    "textContent",
+    "files",
+    "input",
+    "G-JD159TTZNG",
+    "getDownloadURL",
+  ];
+  _0x481e = function () {
+    return _0x705be9;
+  };
+  return _0x481e();
+}
+const storageRef = firebase["storage"]()[_0x19d78b(0x21a)]();
+function getFileList() {
+  const _0x4db061 = _0x19d78b,
+    _0x35bc52 = document[_0x4db061(0x205)]("file-list");
+  (_0x35bc52[_0x4db061(0x21c)] = ""),
+    firebase["firestore"]()
+      [_0x4db061(0x1fd)](_0x4db061(0x1f1))
+      [_0x4db061(0x1fe)]()
+      [_0x4db061(0x20d)]((_0x233e10) => {
+        _0x233e10["forEach"]((_0x47a61a) => {
+          const _0xa7c6ff = _0x5799,
+            _0xecc94 = _0x47a61a[_0xa7c6ff(0x219)](),
+            _0x1425ee = _0xecc94[_0xa7c6ff(0x1f9)],
+            _0x2961b5 = _0xecc94[_0xa7c6ff(0x1f6)],
+            _0x1bb75a = document["createElement"](_0xa7c6ff(0x202));
+          (_0x1bb75a[_0xa7c6ff(0x209)] = _0xa7c6ff(0x21b)),
+            (_0x1bb75a[_0xa7c6ff(0x200)] = "btn\x20btn-success"),
+            _0x1bb75a[_0xa7c6ff(0x1ef)]("click", () => {
+              const _0x44df38 = _0xa7c6ff;
+              storageRef[_0x44df38(0x215)](_0x1425ee)
+                [_0x44df38(0x1f4)]()
+                [_0x44df38(0x20d)]((_0x14ac33) => {
+                  const _0x280014 = _0x44df38;
+                  window[_0x280014(0x21d)](_0x14ac33, _0x280014(0x203));
+                })
+                [_0x44df38(0x207)]((_0x2b69d6) => {
+                  const _0x147368 = _0x44df38;
+                  console[_0x147368(0x208)](_0x2b69d6);
+                });
+            });
+          const _0x2811c6 = _0x35bc52["insertRow"](),
+            _0x3a49a4 = _0x2811c6["insertCell"](),
+            _0x39fa1d = _0x2811c6[_0xa7c6ff(0x1ff)](),
+            _0x5acc2d = _0x2811c6[_0xa7c6ff(0x1ff)]();
+          (_0x3a49a4[_0xa7c6ff(0x1f0)] = _0x1425ee),
+            (_0x39fa1d[_0xa7c6ff(0x1f0)] = _0x2961b5),
+            _0x5acc2d[_0xa7c6ff(0x1f7)](_0x1bb75a);
+        });
+      })
+      [_0x4db061(0x207)]((_0x5d2700) => {
+        const _0x37c8ed = _0x4db061;
+        console[_0x37c8ed(0x208)](_0x5d2700);
+      });
+}
+const searchInput = document[_0x19d78b(0x205)]("search-input");
+searchInput["addEventListener"](_0x19d78b(0x1f2), () => {
+  const _0x34955d = _0x19d78b,
+    _0x53f00d = searchInput[_0x34955d(0x212)][_0x34955d(0x206)](),
+    _0x1333a6 = document[_0x34955d(0x20e)]("#file-list\x20tr");
+  _0x1333a6[_0x34955d(0x1f5)]((_0x3cfc4f) => {
+    const _0x3690a5 = _0x34955d,
+      _0x46f602 = _0x3cfc4f["cells"][0x0][_0x3690a5(0x1f0)]["toLowerCase"](),
+      _0x38c307 =
+        _0x3cfc4f[_0x3690a5(0x1f8)][0x1][_0x3690a5(0x1f0)][_0x3690a5(0x206)]();
+    _0x46f602[_0x3690a5(0x220)](_0x53f00d) || _0x38c307["includes"](_0x53f00d)
+      ? (_0x3cfc4f["style"][_0x3690a5(0x21f)] = _0x3690a5(0x210))
+      : (_0x3cfc4f[_0x3690a5(0x201)][_0x3690a5(0x21f)] = "none");
   });
-});
-
-// Call getFileList() on page load
-getFileList();
+}),
+  getFileList();
